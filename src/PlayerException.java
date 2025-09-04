557d63fc3d89a141cbcfc2d52366d787d955ed60
mkhawa5 <haadi.khawaja1@gmail.com>
2025-09-03 20:49:27 -0400

    Add files via upload

---
+++ src/PlayerException.java
@@ -0,0 +1,17 @@
+public class PlayerException extends Exception {
+    public PlayerException() {
+        super();
+    }
+
+    public PlayerException(String message) {
+        super(message);
+    }
+
+    public PlayerException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public PlayerException(Throwable cause) {
+        super(cause);
+    }
+}
